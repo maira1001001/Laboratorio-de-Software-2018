56b1a492d8d4b4780d242c3532d580aad466edb1
Maira Diaz <mairadiaz@protonmail.ch>
2018-10-24 05:41:33 -0300

    entrega2 finalizada

--- eclipse-workspace/annotations-tp8/Anotaciones/src/ejercicio2/Procesador.java
+++ eclipse-workspace/annotations-tp8/Anotaciones/src/ejercicio2/Procesador.java
@@ -9,12 +9,10 @@ import java.util.HashMap;
 import java.util.Map;
 import java.lang.reflect.*;
 
-public class Procesador	 {
+public class Procesador {
 
 	// pasar a método o constante: Archivo.class y AlmacenarAtributo.class
 
-	
-	
 	public static void mapAndStoreBeanClassToFyleSystem(Class<?> bean) {
 		File file = createFileFromAnnotationBeanClass(bean);
 		HashMap<String, String> fields = getFieldsWithAnnotationsFromBeanObjet(bean);
@@ -28,41 +26,35 @@ public class Procesador	 {
 	}
 
 	private static String defineFileNameBetween(String fileNameFromAnnotationArchivo, String fileNameFromBeanClass) {
-		return fileNameFromAnnotationArchivo.isEmpty() ? fileNameFromBeanClass : fileNameFromAnnotationArchivo;
-		
+		return fileNameFromAnnotationArchivo.isEmpty() ? fileNameFromBeanClass+".txt" : fileNameFromAnnotationArchivo;
+
 	}
 
-	private static HashMap<String, String> getFieldsWithAnnotationsFromBeanObjet(Class bean) {
-		//Mapeado bean = new Mapeado(); // cambiar: se debe instanciar la clase "bean1"
-		Class beanObject = null;
+	private static HashMap<String, String> getFieldsWithAnnotationsFromBeanObjet(Class bean1) {
+		Mapeado beanObject = new Mapeado();
 		HashMap<String, String> result = new HashMap<String, String>();
-		try {
-			beanObject = Class.forName(bean.getName());
-			for (Field field : beanObject.getClass().getDeclaredFields()) {
-				if (field.isAnnotationPresent(AlmacenarAtributo.class)) {
-					try {
-						field.setAccessible(true);
-						result.put(field.getName(), field.get(bean).toString());
-					} catch (IllegalArgumentException | IllegalAccessException e) {
-						System.out.println("El campo " + field.getName()
-								+ " no es accesile o no está definido en la clase" + beanObject.getName());
-					}
+		for (Field field : beanObject.getClass().getDeclaredFields()) {
+			if (field.isAnnotationPresent(AlmacenarAtributo.class)) {
+				try {
+					field.setAccessible(true);
+					result.put(field.getName(), field.get(beanObject).toString());
+				} catch (IllegalArgumentException | IllegalAccessException e) {
+					System.out.println("El campo " + field.getName() + " no es accesile o no está definido en la clase"
+							+ beanObject);
 				}
 			}
-		} catch (ClassNotFoundException e1) {
-			System.out.println("No se pudo crear una instancia de la clase "+ bean.getSimpleName());
 		}
 		return result;
 	}
 
 	private static String[] generateXMLContent(Class bean, HashMap<String, String> fields) {
-		String[] result = new String[fields.size()*2+1];
-		result[0] =  "<nombreClase>" + bean.getSimpleName() + "</nombreClase>" ;
+		String[] result = new String[fields.size() * 2 + 1];
+		result[0] = "<nombreClase>" + bean.getSimpleName() + "</nombreClase>";
 		int i = 1;
 		for (Map.Entry<String, String> field : fields.entrySet()) {
 			result[i] = "<nombreAtributo>" + field.getKey() + "<nombreAtributo>";
-			result[i+1] = "<nombreValor>" + field.getValue() + "<nombreValor>";
-			i=i+2;
+			result[i + 1] = "<nombreValor>" + field.getValue() + "<nombreValor>";
+			i = i + 2;
 		}
 		return result;
 	}
@@ -79,22 +71,23 @@ public class Procesador	 {
 			}
 			bufWriter.flush();
 			fileWriter.close();
-			bufWriter.close(); 
+			bufWriter.close();
 		} catch (IOException e) {
-			System.out.println("El archivo "+ file.getName() + "no pude abrirse o crearse por la siguiente razón:"+ e.getMessage());
+			System.out.println("El archivo " + file.getName() + "no pude abrirse o crearse por la siguiente razón:"
+					+ e.getMessage());
 			e.printStackTrace();
 		} finally {
-			System.out.println("Los datos fueron almacenados en "+file.getAbsolutePath() );
-		} 
+			System.out.println("Los datos fueron almacenados en " + file.getAbsolutePath());
+		}
 	}
-
 
 	public static void main(String[] args) throws Exception {
 		try {
 			Class beanClass = Class.forName(args[0]);
 			mapAndStoreBeanClassToFyleSystem(beanClass);
 		} catch (ClassNotFoundException e) {
-			System.out.println("La clase "+ args[0]+ " NO se encontró. Especifique la clase 'ejercicio2.Mapeado' en la sección 'Run configuration'");
+			System.out.println("La clase " + args[0]
+					+ " NO se encontró. Especifique la clase 'ejercicio2.Mapeado' en la sección 'Run configuration'");
 		}
 	}
 
